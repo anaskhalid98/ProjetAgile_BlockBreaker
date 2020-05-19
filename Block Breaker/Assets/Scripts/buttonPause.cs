6b7e376de973ec78a3e416fe4cd550b60349c774
AnasKhalid <anass.khalid5@gmail.com>
2020-05-19 00:19:12 +0000

    Fix error pause

---
+++ Block Breaker/Assets/Scripts/buttonPause.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonPause : MonoBehaviour
+{
+    bool isPaused = false;
+    
+    public void pauseGame(){
+        if(isPaused){
+            Time.timeScale = 1f;
+            isPaused = false;
+        }else{
+            Time.timeScale = 0f;
+            isPaused = true;
+        }
+    }
+}
